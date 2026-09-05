bfdbf5127d8e291cd468b35d056dfe5457f1da8e
feargodemma234 <feargodemma56@gmail.com>
2026-09-05 12:16:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import streamlit as st
+
 STORE_NAME = "Ebuka Store"
 ADMIN_PASSWORD = "admin123"
 
-SUPABASE_URL = "https://xxxxx.supabase.co" # paste yours
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIs..." # paste anon public key
+# Read from Streamlit Secrets instead of hardcoded
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
